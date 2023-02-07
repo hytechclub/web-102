9784eec0c894daf8d294d4eefcf6bfc70c864926
hylandtechoutreach <hylandtechoutreach@users.noreply.github.com>
2023-02-07 18:58:59 +0000

    Update gitbook 2023-02-07 18:58:59

---
+++ gitbook/gitbook-plugin-scripts/5b9fffafe13ac3cee5d3880ce8c56b27-passwordProtectedPages.js
@@ -0,0 +1,59 @@
+let enabled = true;
+
+let passwordProtectedPages = {
+    "Buttons/SoundboardCodeAlong.md": "fatherhood",
+    "Conditionals/ChatbotCodeAlong.md": "eliza",
+    "DataTypes/SpeedCalcCodeAlong.md": "gottagofast",
+    "DomManipulation/MadLibsCodeAlong.md": "hahaha",
+    "DomManipulationContinued/LyricsCodeAlong.md": "byebyebye",
+    "ForLoops/BabelCodeAlong.md": "infinitemonkeys",
+    "FunctionParameters/DiceRollerCodeAlong.md": "nat20",
+    "HtmlCssReview/HtmlCssReviewCodeAlong.md": "banana"
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    function addPassword(pw) {
+        let containerSection = document.querySelector("section");
+        let containerParent = containerSection.parentElement;
+        containerSection.style.display = "none";
+    
+        let formDiv = document.createElement("div");
+        formDiv.setAttribute("id", "password-form");
+
+        let passwordHeader = document.createElement("h2");
+        passwordHeader.innerHTML = "Password Protected Page";
+
+        let passwordMessage = document.createElement("p");
+        passwordMessage.innerHTML = "Please enter a password to continue.";
+    
+        let passwordInput = document.createElement("input");
+        passwordInput.setAttribute("type", "text");
+        passwordInput.setAttribute("id", "password-input");
+    
+        let submitButton = document.createElement("button");
+        submitButton.innerHTML = "Submit";
+        submitButton.onclick = function () {
+            if (passwordInput.value == pw) {
+                containerSection.style.display = "block";
+                formDiv.style.display = "none";
+            } else {
+                alert("Wrong Password!!!");
+            }
+        }
+    
+        formDiv.appendChild(passwordHeader);
+        formDiv.appendChild(passwordMessage);
+        formDiv.appendChild(passwordInput);
+        formDiv.appendChild(submitButton);
+    
+        containerParent.appendChild(formDiv);
+    }
+
+    gitbook.events.bind("page.change", function() {
+        let passwordForCurrentPage = passwordProtectedPages[gitbook.state.filepath];
+
+        if (enabled && passwordForCurrentPage) {
+            addPassword(passwordForCurrentPage);
+        }
+     });
+});
